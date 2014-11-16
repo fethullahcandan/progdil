dd99e91def1368a8212e7e0fea99ad66941ce7be
Fatih ÇELİKEL <fatih.celikel@bil.omu.edu.tr>
2014-11-16 15:37:23 +0200

    trim metodu kullanarak

--- Underscore.java
+++ Underscore.java
@@ -1,63 +1,54 @@
 import java.util.Scanner;
 
 public class Underscore {
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Kelime girin: ");
-		String veri = s.next();
+		String str = s.next();
 		
-		ReplaceUnderscoreWithSpace(veri);
+		ReplaceUnderscoreWithSpace(str);
 	}
+	
 	static void ReplaceUnderscoreWithSpace(String s) {
 		int uzunluk = s.length();	
 		
 		char[] karakterDizisi = new char[uzunluk];
-		// Girilen kelime uzunluğunda bir karakter dizisi oluşturduk.
 		karakterDizisi = s.toCharArray();	
-		//String nesnemizi karakter dizisine çevirdik ve char tipinde diziye attık.
 		
 		int ilk = IlkIndisBulma(karakterDizisi);
 		int son = SonIndisBulma(karakterDizisi);
 		
-		String dizge = "";
-		for (int i = ilk; i < son; i++) { 
-			dizge = dizge + karakterDizisi[i];
-		}	
+		s = s.replace('_', ' ');
+		s = s.trim();                         //Baştaki ve sondaki boşlukları siler.
 		
-		dizge = dizge.replace( '_', ' ');	
-		
-		char[] altDizi = new char[dizge.length()];
-		altDizi = dizge.toCharArray();
-		
-		for (int i = 0; i < altDizi.length; i++) {
-			karakterDizisi[i+ilk] = altDizi[i];
+		for (int i = 0; i < ilk; i++) {
+			s = "_" + s;
 		}
-		for ( int i = 0; i <uzunluk; i++ ) {
-			System.out.print(karakterDizisi[i]);
+		
+		for (int i = 0; i < uzunluk - son - 1; i++) {
+			s = s + "_";
 		}
-		System.out.println();
+		
+		System.out.println(s);
 	}
+	
 	static int IlkIndisBulma(char[] dizi) {
-		int indis = 0;
-		boolean durum = true;
-		for (int i = 0; i < dizi.length; i++) {
-			if (dizi[i] == '_' && durum == true) 
-				indis++;
-			else
-				durum = false;
+		int i;
+		for (i = 0; i < dizi.length; i++) {
+			if (dizi[i] != '_' ) 
+				break;
+			
 		}
-		return indis;
+		return i;
 	}
+	
 	static int SonIndisBulma(char[] dizi) {
-		int indis = dizi.length-1 ;
-		boolean durum = true;
-		for (int i = dizi.length-1; i >= 0; i--) {
-			if (dizi[i] == '_' && durum == true)
-				indis--;
-			else
-				durum = false;
+		int j;
+		for (j = dizi.length-1; j >= 0; j--) {
+			if (dizi[j] != '_' )
+				break;
 		}
-		return indis;
+		return j;
 	}
 }
 //String sınıfı metotları, immutable metotlardır.
