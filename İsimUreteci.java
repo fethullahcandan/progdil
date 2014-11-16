3bb7e82efe565f27166c4a27a14808178165b834
Fatih ÇELİKEL <fatih.celikel@bil.omu.edu.tr>
2014-11-17 00:00:35 +0200

    dil kontrolü ve sözlüğün düzenlenmesi

--- İsimUreteci.java
+++ İsimUreteci.java
@@ -1,5 +1,3 @@
-package progDil;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -8,176 +6,182 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class IsimUreteci {
-
-	public static void main (String[] args)
-	{
+	public static void main (String[] args)	{
 		Scanner s = new Scanner(System.in);
-		System.out.println("Dil seçiniz: ");
-		String dil = s.next();
-		System.out.println("Lutfen üretilecek isim sayısını giriniz: ");
-		int sayi =s.nextInt();
-
-		isimUret(dil, sayi);
+		boolean durum = false;
+		String dil;
+		int sayi;
+		
+		do {
+			System.out.println("Dil seçin Türkçe için tr İngilizce için en");
+			dil = s.next();
+			System.out.println("Lütfen üretilecek isim sayısını girin ");
+			sayi = s.nextInt();
+			if(dil.equals("tr") || dil.equals("en"))
+				durum = true;
+			else {
+				System.out.println("Yanlış dil seçtin");
+			}	
+		} while(durum != true);
+		
+		IsimUret(dil, sayi);
 	}
 
-	static void isimUret(String Language, int Adet) {
+	static void IsimUret(String dil, int sayi) {
 		String[]
-				TrAdlar = {
-				/*"Deniz",
-				"Akşam",
-				"Kitap",
-				"Ev",
-				"Öğrenci",
-				"Gün",
-				"Yumruk",			
-				"Kurt",
-				"Bilezik",
-				"Yılan",
-				"Aslan",
-				"Masa",
-				"Top",
-				"Vazo",
-				"Çorap",
-				"Duvar",
-				"Çanta",
-				"Tasma",
-				"Yagmur",
-				"Şimşek",
-				"Çilek",
-				"Koku",
-				"Korsan",
-				"Bıçak",*/
-				"Çivi",
-				"Fethullah",
-				"Fatih",
-		},
-
-		EngAdlar = {
-
-				"Sea",
-				"Night",
-				"Book",
-				"House",
-				"Student",
-				"Day",
-				"Punch",
-				"Wolf",
-				"Bracelet",
-				"Snake",
-				"Lion",
-				"Table",
-				"Ball",
-				"Vase",
-				"Socks",
-				"Wall",
-				"Bag",
-				"Collar",
-				"Rain",
-				"Lightning",
-				"Strawberry",
-				"Smell",
-				"Pirate",
-				"Knife",
-				"Gib",
-				"Fethullah",
-				"Fatih"
-		},
-
-		TrSifatlar = {
-				/*"İyi",
-				"Kötü",
-				"Çirkin",
-				"Mutlu",
-				"Sağlıklı",
-				"Yardımsever",
-				"Sevimli",
-				"Mükemmel",
-				"Gururlu",
-				"Güzel",
-				"Sadık",
-				"Muhteşem",
-				"Demir",
-				"Başarılı",
-				"Harika",
-				"Neşeli",*/
-				"Şanslı",
-				"Esprili",
-				"Kızgın",
-		},
-
-		EngSifatlar = {
-				"Good",
-				"Bad",
-				"Ugly",
-				"Happy",
-				"Healthy",
-				"Helpful",
-				"Lovely",
-				"Perfect",
-				"Proud",
-				"Nice",
-				"Obedient",	
-				"Splendid",
-				"Iron",
-				"Successful",
-				"Wonderful",
-				"Vivacious",
-				"Lucky",
-				"Witty",
-				"Angry",
-		};
+				isimler = {
+					"abajur",
+					"baca",
+					"canavar",
+					"çaba",
+					"demeç",
+					"el",
+					"fakülte",
+					"gaflet",
+					"hatıra",
+					"ıhlamur",
+					"iade",
+					"jandarma",
+					"kaş",
+					"labirent",
+					"medeniyet",
+					"nabız",
+					"ocak",
+					"öncü",
+					"puan",
+					"rahat",
+					"sene",
+					"şablon",
+					"tesis",
+					"ucube",
+					"ücret",
+					"vaat",
+					"yoğurt",
+					"zehir",
+				},
+				sifatlar = {
+					"ait",
+					"basit",
+					"cıvık",
+					"çabuk",
+					"dinamik",
+					"erişilebilir",
+					"feci",
+					"gri",
+					"hoyrat",
+					"ılık",
+					"iki",
+					"kaplı",
+					"lacivert",
+					"müstakil",
+					"nitel",
+					"olağan",
+					"öncül",
+					"patlayıcı",
+					"rafadan",
+					"sade",
+					"şık",
+					"tatlı",
+					"uçan",
+					"ücra",
+					"vasat",
+					"yıkık",
+					"zayıf",
+				},
+				names = {
+					"ability",
+					"bigot",
+					"cab",
+					"dab",
+					"eagle",
+					"fable",
+					"gable",
+					"haberdasher",
+					"ice",
+					"jackal",
+					"karat",
+					"lab",
+					"machine",
+					"nail",
+					"oak",
+					"pace",
+					"quack",
+					"rabbit",
+					"saber",
+					"tabby",
+					"ulcer",
+					"vacancy",
+					"wad",
+					"xenophobia",
+					"yacht",
+					"zinc",
+				},
+				adjectives = {
+					"akin",
+					"beatiful",
+					"conscious",
+					"damned",
+					"each",
+					"fair",
+					"gangling",
+					"habitual",
+					"idle",
+					"jocular",
+					"known",
+					"laconic",
+					"macabre",
+					"nasty",
+					"oblique",
+					"pugnacious",
+					"quadruple",
+					"radical",
+					"sad",
+					"tactful",
+					"ubiqutous",
+					"vague",
+					"warlike",
+					"yellow",
+					"zigzag",
+				};
 
 		String[] Adlar = null, Sifatlar = null;
-		//ArrayList<String> Uretilenler = null;
-
-		if (Language.equals("Tr")) {
-			Adlar = TrAdlar;
-			Sifatlar = TrSifatlar;
+	
+		if (dil.equals("tr")) {
+			Adlar = isimler;
+			Sifatlar = sifatlar;
 		}
-		else if (Language.equals("Eng")) {
-			Adlar = EngAdlar;
-			Sifatlar = EngSifatlar;
+		else if (dil.equals("en")) {
+			Adlar = names;
+			Sifatlar = adjectives;
 		}
-		else
-		{
-			System.out.println("Seçiminiz yanlış");
-		}
-
-		Random rnd = new Random();
-		int j=0;
-		int olasilik = Sifatlar.length*Adlar.length;
-		System.out.println(olasilik);
 		
-		for (int i = 0; i < Adet;) {
+		Random rnd = new Random();
+		int j = 0;
+		int olasilik = Sifatlar.length*Adlar.length;
+		
+		for (int i = 0; i < sayi;) {
 			
 			int AdIndex = rnd.nextInt(Adlar.length);
 			int SifatIndex = rnd.nextInt(Sifatlar.length);
 			
-			String ifade = Sifatlar[SifatIndex]+" "+Adlar[AdIndex];
-			File f = new File("d:\\ProgDil\\" + ifade);
+			String ifade = Sifatlar[SifatIndex]+"-"+Adlar[AdIndex];
+			File f = new File("/home/celikel/GitHub/progdil/" + ifade);
 			
-			File s =new File("d:\\ProgDil");
-			int sayi = s.list().length;
-			//System.out.println("Dosya sayisi = "+ sayi);
+			File s = new File("/home/celikel/GitHub/progdil/");
+			int dosya_sayisi = s.list().length;
 			
-			if(f.isDirectory())
-			{
-				System.out.println(ifade+ " adlı klasör bulunmaktadır.");
+			
+			if(f.isDirectory()) {
 				j += 1;
-				if(sayi == olasilik)
-				{
-					System.out.println("Boş= "+ j);
+				if(dosya_sayisi == olasilik)
 					break;
-				}
 			}
-			else
-			{
+			else {
 				System.out.println((i+1)+". Üretilen ==> "+ ifade);
-				File klasor = new File("d:\\ProgDil\\" + ifade);
+				File klasor = new File("/home/celikel/GitHub/progdil/" + ifade);
 				klasor.mkdir();	
 				i++;
 			}
 		}
 	}
 }
-
